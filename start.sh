6ef7fed38d4fe8b60c93072008dd7ba0bdafc164
mldreiling14 <mldreil@ad.unc.edu>
2025-09-22 21:17:26 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ docker build . -t bios611-r
 
 docker run -d --name bios611-r \
 -e PASSWORD=password \
--v "/c/Users/drdre/OneDrive/BIOS611:/home/rstudio/project" \
--v "/c/Users/drdre/OneDrive/BIOS611/.ssh:/home/rstudio/.ssh" \
--v "/c/Users/drdre/OneDrive/BIOS611/.gitconfig:/home/rstudio/.gitconfig" \
+-v "/c/Users/drdre/BIOS611:/home/rstudio/project" \
+-v "/c/Users/drdre/BIOS611/.ssh:/home/rstudio/.ssh" \
+-v "/c/Users/drdre/BIOS611/.gitconfig:/home/rstudio/.gitconfig" \
 -p 8787:8787 \
 rocker/verse
