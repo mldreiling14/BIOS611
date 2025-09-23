4a4794ecb5409d2ccb90957bf76c3eed3d7b7f55
mdreiling14 <mdreiling14@gmail.com>
2025-09-23 11:06:06 -0400

    Updated path on shell file

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-export MSYS_NO_PATHCONV=1
+MSYS_NO_PATHCONV=1
 
-docker build . -t bios611-r
+docker build . -t bios611
 
-docker run -d --name bios611-r \
+docker run -d --name bios611 \
 -e PASSWORD=password \
--v "/c/Users/drdre/OneDrive/BIOS611:/home/rstudio/project" \
--v "/c/Users/drdre/OneDrive/BIOS611/.ssh:/home/rstudio/.ssh" \
--v "/c/Users/drdre/OneDrive/BIOS611/.gitconfig:/home/rstudio/.gitconfig" \
+-v //c/Users/drdre/BIOS611:/home/rstudio/project \
+-v //c/Users/drdre/BIOS611/.ssh:/home/rstudio/.ssh \
+-v //c/Users/drdre/BIOS611/.gitconfig:/home/rstudio/.gitconfig \
 -p 8787:8787 \
 rocker/verse
