6cacd80f53322cba5048ca13be30c97d02e950e3
mdreiling14 <mdreiling14@gmail.com>
2025-09-29 10:52:01 -0400

    Updates from Surface

--- UFO_Sightings_Assignment/UFO_Assignment.R
+++ UFO_Sightings_Assignment/UFO_Assignment.R
@@ -3,3 +3,15 @@ setwd("/home/rstudio/project/UFO_Sightings_Assignment")
 
 data <- read.csv('nuforc_sightings.csv')
 
+unique(data$state)
+
+US_data <- function(x){
+  x |> mutate(
+    country = str_to_upper(country),
+    state = str_to_upper(state),
+    shape = str_to_lower(str_trim(shape))
+  )
+   data.frame(
+     
+   )
+}
